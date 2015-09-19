f5d974efa5108aa0370e784a696906d2cabc94bb
MajorGrits <casey.dalton.brown@hotmail.com>
2015-09-19 16:08:24 -0500

    Initial Commit

---
+++ QuickSightProject/Public/Scripts/core.js
@@ -0,0 +1,22 @@
+﻿//public/Scrpts/core.js
+
+var app = angular.module('QuickSight', []);
+
+app.controller('mainController', function($scope, $http) {
+    
+    $scope.testText = "Press the button!";
+
+    $scope.changeText = function () {
+        $scope.testText = "Pressed";
+    };
+});
+
+app.controller('infoController', function ($scope, $http) {
+    $scope.developers = [
+        { name: 'Casey' },
+        { name: 'Ben' },
+        { name: 'Anthony' },
+        { name: 'Spencer' },
+        { name: 'Zach' }
+    ];
+});
