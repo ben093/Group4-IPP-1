54e8e3a5d6e46beb0ef1d8a58c19e9d6f6103898
MajorGrits <casey.dalton.brown@hotmail.com>
2015-10-01 23:10:11 -0500

    fixing merger errors

--- QuickSightProject/server.js
+++ QuickSightProject/server.js
@@ -1,25 +1,18 @@
 ﻿// set up requires
 var express = require('express');
-<<<<<<< HEAD
+
 var path = require('path');
 var app = express();                               // create our app w/ express
 var morgan = require('morgan');                    // log requests to the console (express4)
 var bodyParser = require('body-parser');           // pull information from HTML POST (express4)
 var methodOverride = require('method-override');   // simulate DELETE and PUT (express)
-=======
+
 var app = express();                               // create our app w/ express
 var morgan = require('morgan');                    // log requests to the console (express4)
 var bodyParser = require('body-parser');           // pull information from HTML POST (express4)
 var methodOverride = require('method-override');   // simulate DELETE and PUT (express4)
 var path = require('path');
 
->>>>>>> ed0e3b75303a5519cc924b5ca7b769d31e30dc71
-
-//var mongodb = require('mongodb');                  // Mongo Database
-//var mongoClient = mongodb.MongoClient;
-//var mongoose = require('mongoose');                // Will handle mongoDB
-
-//var url = 'mongodb:://localhost:27017/test';       //mongoDB url (localhost for testing)
 
 // configuration
 app.use(express.static(__dirname + '/public'));           // set the static files location /public/img will be /img for users
@@ -32,8 +25,6 @@ app.use(methodOverride());
 // listen (start app with node server.js) ======================================
 app.listen(8080);
 console.log("App listening on port 8080");
-
-<<<<<<< HEAD
 
 
 //Primary routes
@@ -51,18 +42,4 @@ app.get('/hscores', function (req, res) {
 
 app.get('/game', function (req, res) {
     res.sendFile(path.join(__dirname, '/Public/Views/game.html'));
-=======
-app.get('*', function (req, res) {
-    res.sendfile(path.join(__dirname, '/Public/Views/base.html'));
->>>>>>> ed0e3b75303a5519cc924b5ca7b769d31e30dc71
 });
-
-//Connect to database and do some simple insertion and deletion calls
-//MongoClient.connect(url, function (err, db) {
-//    if (err) {
-//        console.log('Unable to connect');
-//    } else {
-//        console.log("Connected to ", url);
-//        db.close(); //gotta change path in dbpath mongo
-//    }
-//});
