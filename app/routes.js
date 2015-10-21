08bd23975ceff797bf7b559e0850f7ca8ab0ad58
Casey <casey.dalton.brown@hotmail.com>
2015-10-21 10:43:02 -0500

    v1.181

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 // app/routes.js
 
 // grab the nerd model we just created
+var path = require('path');
 
 module.exports = function(app) {
 	
@@ -10,7 +11,7 @@ module.exports = function(app) {
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
-		res.sendfile('./public/views/index.html'); // load our public/index.html file
+		res.sendFile(path.join(__dirname , '../public','views/index.html')); // load our public/index.html file
 	});
 
 };
